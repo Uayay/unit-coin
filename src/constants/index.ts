fe558d094f4e9f3f2a0debb776e2feda0842c981
雪地·杰纳瑞 <565663700@qq.com>
2021-04-26 18:34:04 +0800

    Create index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const UNITS = [
+	'', 'k', 'm', 'b', 't',
+	'aa', 'bb', 'cc', 'dd', 'ee', 'ff',
+	'gg', 'hh', 'ii', 'jj', 'kk', 'll',
+	'mm', 'nn', 'oo', 'pp', 'qq', 'rr',
+	'ss', 'tt', 'uu', 'vv', 'ww', 'xx',
+	'yy', 'zz'
+]
