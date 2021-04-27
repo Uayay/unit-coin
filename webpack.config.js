abe83692cd4bbf32c0aea50a298da4a60a83ac78
雪地·杰纳瑞 <565663700@qq.com>
2021-04-27 17:50:11 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const NODE_ENV = process.env.NODE_ENV
+const isEnvDevelopment = NODE_ENV === 'development'
+const isEnvProduction = NODE_ENV === 'production'
 
 module.exports = {
-  mode: 'production',
-  entry:'./src/index.ts',
-  output:{
-    filename:'dist/index.js'
+  entry:  path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    publicPath: '.',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd'
   },
-  module:{
+  mode: isEnvProduction ? 'production' : 'development',
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    mainFiles: ['index'],
+  },
+  module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {}
+        loader: 'ts-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
 }
