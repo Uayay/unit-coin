4508954b3529e666c5ed657d47b07798e67be618
雪地·杰纳瑞 <565663700@qq.com>
2021-04-28 17:59:11 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { UNITS } from "./constants"
 
 function unitCoin() {
   const unitsValue = UNITS.map((item, index) => {
-    return [item, 10 ** index]
+    return [item, 10 ** (index * 3)]
   })
-
+  
   return unitsValue
 }
 
