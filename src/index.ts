2c4a1ad38001401869b85fe413cae208cd3eeef6
雪地·杰纳瑞 <565663700@qq.com>
2021-04-28 18:48:07 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { UNITS } from "./constants"
 
-function unitCoin() {
-  const unitsValue = UNITS.map((item, index) => {
-    return [item, 10 ** (index * 3)]
-  })
-  
-  return unitsValue
+function unitCoin(param: any): string {
+  const origVal = Number(param)
+
+  if (!Number.isNaN(origVal)) {
+    throw new Error('The param type must be a number.')
+  }
+
+  let transVal = ''
+
+  for (let i = 0; i < UNITS.length; i++) {
+    const unit = UNITS[i]
+    const norms = 10 ** (i * 3)
+
+    if (
+      (i === UNITS.length - 1) ||
+      (origVal >= norms && norms < norms * 10 ** 3)
+    ) {
+      transVal = `${(origVal / norms).toFixed(1)}${unit}`
+      return transVal
+    }
+  }
+
+  return transVal
 }
 
 export { unitCoin }
