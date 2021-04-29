25c7ef2b2990d6ad948800c77ba18b64fdf8f65f
雪地·杰纳瑞 <565663700@qq.com>
2021-04-29 17:50:50 +0800

    Update index.test.ts

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,5 @@
 import { unitCoin } from '../src'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+test('NaN', () => {
+  expect(unitCoin('NaN')).toEqual({ error: 'The param type must be a number.' });
 });
