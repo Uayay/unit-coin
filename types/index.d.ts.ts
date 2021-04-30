3f8cc469980a3ab83227fb588da2b88c300798a4
辣麻多拿 <565663700@qq.com>
2021-05-01 02:05:16 +0800

    .

--- types/index.d.ts.ts
+++ types/index.d.ts.ts
@@ -1,5 +1,7 @@
-declare namespace unitCoin {
-  export function unit(param: string | number): string
-}
+// declare namespace unitCoin {
+//   export function unit(param: string | number): string
+// }
 
-export default unitCoin
+// export default unitCoin
+
+export let unitCoin: (param: string | number) => string
