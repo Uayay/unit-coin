f9c7028162eef7528c510cc0973ff069e317201f
雪地·杰纳瑞 <565663700@qq.com>
2021-04-30 16:18:18 +0800

    Update index.test.ts

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,23 +1,32 @@
-import { unitCoin } from '../src'
+import unitCoin from '../src'
 
 test('NaN', () => {
   expect(() => {
-    unitCoin('NaN')
+    unitCoin.unit('NaN')
   }).toThrow('The param type must be a number.')
 })
 
 test('0.1 to 0.1', () => {
-  expect(unitCoin(0.1)).toBe('0.1')
+  expect(unitCoin.unit(0.1))
+    .toBe('0.1')
 })
 
 test('1 to 1', () => {
-  expect(unitCoin(1)).toBe('1')
+  expect(unitCoin.unit(1))
+    .toBe('1')
 })
 
 test('999 to 999', () => {
-  expect(unitCoin(999)).toBe('999')
+  expect(unitCoin.unit(999))
+    .toBe('999')
 })
 
-test('1024 to 1.0k', () => {
-  expect(unitCoin(1024)).toBe('1.0k')
+test('1024 to 1.02k', () => {
+  expect(unitCoin.unit(1024))
+    .toBe('1.02k')
+})
+
+test('1024000 to 1.02m', () => {
+  expect(unitCoin.unit(1024000))
+    .toBe('1.02m')
 })
