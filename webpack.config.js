b6811422277385bc6c44392722a404f1a4575a57
辣麻多拿 <565663700@qq.com>
2021-04-30 20:33:28 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-
 const NODE_ENV = process.env.NODE_ENV
 const isEnvDevelopment = NODE_ENV === 'development'
 const isEnvProduction = NODE_ENV === 'production'
