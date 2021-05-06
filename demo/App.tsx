211e1ee5cab44482bbac9f37966411a10d92461d
雪地·杰纳瑞 <565663700@qq.com>
2021-05-06 16:46:52 +0800

    Create App.tsx

---
+++ demo/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from 'antd'
+
+function App() {
+  return (
+    <div className="App" style={{maxWidth: '1024px', margin: 'auto'}}>
+      <Input />
+    </div>
+  )
+}
+
+export default App
