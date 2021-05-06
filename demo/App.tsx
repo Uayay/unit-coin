6914d7490c48ebf3d3d0645c3d6781ad4c736a26
辣麻多拿 <565663700@qq.com>
2021-05-06 21:58:34 +0800

    .

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,17 +1,35 @@
-import React, { ChangeEvent } from 'react'
-import { Input } from 'antd'
+import React, { ChangeEvent, useState } from 'react'
+import { Input, Typography, Form } from 'antd'
 import { unitCoin } from '../src/index'
+const { Title } = Typography
 
 function App() {
+  const [result, setResult] = useState('')
+
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
     const num = Number(value)
-    console.log(unitCoin(num))
+
+    try {
+      setResult(unitCoin(num))
+    } catch (error) {
+      setResult(error.message)
+    }
   }
 
   return (
-    <div className="App" style={{maxWidth: '1024px', margin: 'auto'}}>
-      <Input onChange={onChange} />
+    <div className="App" style={{maxWidth: '600px', margin: 'auto'}}>
+      <Title level={2}>UnitCoin</Title>
+
+      <Form>
+        <Form.Item label="输入值">
+          <Input onChange={onChange} />
+        </Form.Item>
+
+        <Form.Item label="结果">
+          {result}
+        </Form.Item>
+      </Form>
     </div>
   )
 }
