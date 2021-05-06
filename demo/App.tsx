97c3c8aca9e8f91bfa3ac8cb563deea72458f4dd
雪地·杰纳瑞 <565663700@qq.com>
2021-05-06 18:36:40 +0800

    Update App.tsx

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import { Input } from 'antd'
+import { unitCoin } from '../src/index'
 
 function App() {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    const num = Number(value)
+    console.log(unitCoin(num))
+  }
+
   return (
     <div className="App" style={{maxWidth: '1024px', margin: 'auto'}}>
-      <Input />
+      <Input onChange={onChange} />
     </div>
   )
 }
